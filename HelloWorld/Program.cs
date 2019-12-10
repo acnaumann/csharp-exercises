209f7bffbf8f554ef079405800e7ade954556877
acnaumann <alysson12985@gmail.com>
2019-12-10 13:35:05 -0600

    * Program.cs: test commit
    * Program.cs: test commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int area = width * height;
+
+            Console.WriteLine("The area of the rectangle is: " + area);
+
+            Console.WriteLine("Enter the temperature in Fahrenheit: ");
+            double fahrenheit = double.Parse(Console.ReadLine());
+
+            double celsius = (fahrenheit - 32) * (5.0 / 9.0);
+
+            Console.WriteLine("The temperature in C is: " + (int)(celsius));
+
+        }
+    }
+}
--- IntroDemo/Program.cs
+++ IntroDemo/Program.cs
@@ -6,7 +6,7 @@ namespace IntroDemo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test!");
         }
     }
 }
