421accd7f946cd942502e6b622aa9d3a701e01c0
acnaumann <alysson12985@gmail.com>
2019-12-10 13:32:04 -0600

    * Test.cs: * Program.cs: * Test.csproj: * test2.csproj: * Program.cs: * Test.cs: * packages.config: * csharp-exercises.sln: * Program.cs: * Strings.csproj: * Program.cs: * IntroDemo.csproj: * packages.config: * packages.config: * InputOutput.csproj: * AssemblyInfo.cs: * NumericTypes.csproj: * AssemblyInfo.cs: * AssemblyInfo.cs: * AssemblyInfo.cs: * AssemblyInfo.cs:
    * HelloWorld.csproj: Unit 3 Class 1 Exercises
    
    * AssemblyInfo.cs: Unit 3 Class 1

---
+++ InputOutput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Project
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello " + input + "!");
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string Alice = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
+                "and of having nothing to do: once or twice she had peeped into the book her sister was reading, " +
+                "but it had no pictures or conversations in it, 'and what is the use of a book,' " +
+                "thought Alice 'without pictures or conversation?'";
+
+            Console.WriteLine("Enter a phrase to check: ");
+
+            string input = Console.ReadLine();
+
+            
+
+            if (Alice.Contains((input)))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            //Console.WriteLine(first_sentence);
+            //Console.ReadLine();
+
+        }
+    }
+}
