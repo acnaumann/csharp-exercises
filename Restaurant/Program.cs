74012d166a3d72b8cc875a735b1358a34756ef12
acnaumann <alysson12985@gmail.com>
2019-12-19 14:43:20 -0600

    Restaurant Menu Continued

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Restaurant
 {
@@ -20,6 +21,47 @@ namespace Restaurant
             Categories = categories;
             DateAdded = added;
 
+        }
+
+        public bool IsNew(DateTime fromWhen)
+        {
+            //if (fromWhen == null)
+            //{
+            //    return false;
+            //}
+
+            if (DateTime.Compare(fromWhen, DateAdded) < 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += Name + " - " + Description +
+                "\n$" + Price +
+                "\nCategories: " + String.Join(", ", Categories);
+
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            MenuItem itemObj;
+
+            if (obj == null || (obj.GetType() != this.GetType()))
+            {
+                return false;
+            }
+
+            itemObj = obj as MenuItem;
+            return itemObj.Name == this.Name;
 
         }
 
@@ -34,14 +76,85 @@ namespace Restaurant
         public Menu(string name, List<MenuItem> items, DateTime date)
         {
             Name = name;
-            Items = items;
+
+            if (items == null)
+            {
+                this.Items = new List<MenuItem>();
+            }
+            else
+            {
+                this.Items = items;
+            }
+            
             LastUpdated = date;
-
-
         }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            DateTime now = DateTime.Now;
+
+            if (item == null)
+            {
+                //Maybe log an error message
+                return;
+            }
+
+            Items.Add(item);
+            LastUpdated = now;
+            item.DateAdded = now;
+        }
+
+        public void RemoveMenuItem(MenuItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            Items.Remove(item);
+        }
+
+        //public ListMenuItems()
+        //{
+        //    return;
+        //}
+
+        //public ListMenu()
+        //{
+        //    return;
+        //}
+
+        public override bool Equals(object obj)
+        {
+            Menu menuObj;
+
+            if (obj == null || (obj.GetType() != this.GetType()))
+            {
+                return false;
+            }
+
+            menuObj = obj as Menu;
+
+            return menuObj.Name == this.Name && menuObj.Items.SequenceEqual(this.Items);
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += Name + " Menu\n" +
+                String.Join("\n------\n", Items);
+
+            return str;
+        }
+        //private void bool IsNew(MenuItem item)
+        //{
+        //    if (item.DateAdded > (DateTime.Now))
+        //    {
+        //        return true;
+        //    }
+
+        //}
     }
-
-
 
     class MainClass
     {
@@ -62,7 +175,7 @@ namespace Restaurant
 
             Menu resMenu = new Menu(
                 "That One Place",
-                new List<MenuItem>() { pasta, tiramisu },
+                new List<MenuItem>() { pasta, tiramisu },    //what happens if you pass in null here? where is the null exeption?
                 new DateTime()
                 );
 
@@ -73,11 +186,12 @@ namespace Restaurant
             resMenu.Items[1].Price = 7.99;
 
             Console.WriteLine(resMenu.Items[1].Name + " " + resMenu.Items[1].Price);
-            
+            Console.WriteLine("--------");
+            Console.WriteLine(tiramisu); 
+            Console.WriteLine(tiramisu.Equals(new MenuItem("Tiramisu", "desc", 4.99, null, DateTime.Now)));
+
+            Console.WriteLine(resMenu);
 
         }
-
-
     }
-
 }
